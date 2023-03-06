d63121945670e675df098414cf7a04b10e3f50a2
ZahFazz <92514556+ZahFazz@users.noreply.github.com>
2023-03-06 21:05:35 +0700

    Add files via upload

---
+++ week 2/scipyVsNativeMethode.py
@@ -0,0 +1,12 @@
+#scipy and native method
+
+from scipy import stats
+import statistics as s
+
+value = [70, 80, 70, 70, 90, 100]
+
+value_mode = stats.mode(value) #scipy
+modus = s.mode(value) #Native Python
+print(modus)
+print(value_mode)
+print(value_mode.mode)
